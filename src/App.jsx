1c5374b639ac63b01696a0fff0d0eec6806746ce
rabeahmed2002 <mrabe2024@gmail.com>
2024-03-11 01:16:53 +0500

    timer challenge component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+
+        <TimerChallenge 
+          title="easy" 
+          targetTime={1}
+        />
+
+        <TimerChallenge 
+          title="medium" 
+          targetTime={1}
+        />
+
+        <TimerChallenge 
+          title="hard" 
+          targetTime={1}
+        />
+
+      </div>
     </>
   );
 }
