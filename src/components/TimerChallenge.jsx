76faa91974669ae998f713c5de30434d8ac6e389
rabeahmed2002 <mrabe2024@gmail.com>
2024-03-11 01:38:20 +0500

    timer error resolved

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 const TimerChallenge = ({title, targetTime}) => {
+
+    const timer=useRef()
+
+    const [timerStarted, setTimerStarted] = useState(false) 
+    const [timerExpired, setTimerExpired] = useState(false) 
+    
+    const handleStart =() => {
+        timer.current=setTimeout(()=>{
+            setTimerExpired(true)
+        }, targetTime * 1000)
+
+        setTimerStarted(true)
+    }
+
+    const handleStop = () => {
+        clearTimeout(timer.current);
+    }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+
+        {timerExpired && <p>You lost</p>}
+
       <p className='challenge-time'>
         {targetTime} second{targetTime > 1 ? "s":""}
       </p>
       <p>
-        <button>
-            Start challenge
+        <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "stop":"start"} challenge
         </button>
       </p>
-      <p className='active'>
-        Time is running/inactive
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? "Time is running" : "Timer inactive"}
       </p>
     </section>
   )
